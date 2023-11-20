0a1141eb8ce4ec079a36310de92ff52cd1a46660
Oliver <117604613+OliverHu726@users.noreply.github.com>
2023-11-20 15:50:45 -0500

    Create dh3517_app.py

---
+++ dh3517_app.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+file_path = "https://raw.githubusercontent.com/OliverHu726/ML_in_FRE_HW5_Q1/main/submissions.csv"
+df = pd.read_csv(file_path)
+
+Accurate_status = []
+for n in range(len(df)):
+    if df["Ground Truth"][n] == df["Predictions"][n]:
+        Accurate_status.append(1)
+    else:
+        Accurate_status.append(0)
+total_acc = sum(Accurate_status) / len(Accurate_status)
+
+def calculate_accuray(data, category):
+  acc_metric = data.groupby(category).mean()[category]
+  return acc_metric
+  
+st.title('Titanic ML Project Analysis')
+# Dropdown menu
+selected_category = st.selectbox('Select a category:', ['Sex', 'Pclass'])
+# Calculate accuracy based on the selected category
+group_acc_metric = calculate_accuray(selected_category)
+# Display results
+st.write(f'Total Accuracy: {total_acc}')
+st.write(f'Per-{selected_category} Accuracy: {group_acc_metric}')
