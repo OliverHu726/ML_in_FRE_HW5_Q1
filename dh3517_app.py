7e5f70565448040ef2424c6a7f5a1614ce63a890
Oliver <117604613+OliverHu726@users.noreply.github.com>
2023-11-21 14:52:44 -0500

    Update dh3517_app.py

--- dh3517_app.py
+++ dh3517_app.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import numpy as np
 import streamlit as st
 
 file_path = "https://raw.githubusercontent.com/OliverHu726/ML_in_FRE_HW5_Q1/main/submissions.csv"
 df = pd.read_csv(file_path)
+df['Pclass'] = df['Pclass'].astype(str)
 
 Accurate_status = []
 for n in range(len(df)):
@@ -11,14 +13,13 @@ for n in range(len(df)):
     else:
         Accurate_status.append(0)
 df["Accurate"] = Accurate_status
-df["Pclass"] = df["Pclass"].astype(str)
 total_acc = sum(Accurate_status) / len(Accurate_status)
 
 def calculate_accuracy(category):
-  metric = df.groupby(category).mean()["Accurate"]
-  return metric
+  acc_metric = df.groupby(category).mean()["Accurate"]
+  return acc_metric
   
-st.title('Titanic ML Project Analysis')
+st.title('dh3517_ML_in_FRE_HW5_Q1')
 # Dropdown menu
 selected_category = st.selectbox('Select a category:', ['Sex', 'Pclass'])
 # Calculate accuracy based on the selected category
